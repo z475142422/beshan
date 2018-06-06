4dd31f3114fc309bcf3979852261592b9a32fbf1
zheng-lei <2933512646@qq.com>
2018-06-06 21:37:23 +0800

    6.6 modify meiguo-dongan-gaoxiao-yishuguan-lvxing.htm meiguo-dongan-gaoxiao-yishuguan-lvxing.css meiguo-dongan-gaoxiao-yishuguan-lvxing.jsl

--- js/meiguo-dongan-gaoxiao-yishuguan-lvxing.js
+++ js/meiguo-dongan-gaoxiao-yishuguan-lvxing.js
@@ -1,5 +1,5 @@
 $(function(){
-	window.onresize=resizeBannerImage;//当窗口改变宽度时执行此函数
+	window.onresize = resizeBannerImage;//当窗口改变宽度时执行此函数
 	function resizeBannerImage()
  	{
    		var winW = $(window).width();
@@ -9,5 +9,45 @@ $(function(){
    		else{
    			$('.about').css('display','block')
    		}
+      if(winW<768){
+        $('.large').css('border','none')
+        $('.transport').css('border-top','1px solid black')
+      }
+      else{
+        $('.large').css('border-right','1px solid black')
+        $('.transport').css('border','none')
+      }
   	}
 })
+
+window.onload = function(){
+  for(var i=0;i<4;i++){
+    var canv = document.getElementsByClassName('canva')[i];
+    var con = canv.getContext("2d");
+    con.beginPath();
+    con.lineWidth = 3;
+    con.moveTo(0,2);
+    con.lineTo(130,2);
+    con.lineTo(145,50);
+    con.lineTo(160,2);
+    con.lineTo(300,2)
+    con.stroke();
+  }
+  var can = document.getElementsByClassName('canv')[0];
+  var con1 = can.getContext("2d");
+  con1.beginPath();
+  con1.lineWidth = 3;
+  con1.strokeStyle = '#f0f1d9';
+  con1.moveTo(0,2);
+  con1.lineTo(130,2);
+  con1.lineTo(145,50);
+  con1.lineTo(160,2);
+  con1.lineTo(300,2)
+  con1.stroke();
+  // var w = $(window).width();
+  // var h = $(window).height();
+  // var modalContent = document.getElementsByClassName('modal-dialog')[0];
+  // modalContent.style.width = w + 'px';
+  // modalContent.style.height = h + 'px';
+  // modalContent.style.marginTop = '0px';
+}
